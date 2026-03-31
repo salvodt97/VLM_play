ff4103a3bdd37c1688f21a3dcb571ec2214e338d
Salvatore Della Torca <s.dellatorca@gmail.com>
2026-03-31 14:13:21 +0200

    ADD: codifica il modello gemma

--- LLM/gemma.py
+++ LLM/gemma.py
@@ -3,7 +3,7 @@ from re import S
 from tkinter import HIDDEN
 from turtle import position
 
-from numpy import pad
+from numpy import dtype, pad
 import torch
 from torch import device, nn
 from torch.nn import CrossEntropyLoss
@@ -80,6 +80,48 @@ class PaliGemmaConfig:
         
         self.text_config.num_image_tokens = (self.vision_config.image_size // self.vision_config.patch_size) ** 2
         self.vision_config.projection_dim = projection_dim
+        
+        
+
+class GemmaModel(nn.Module):
+    def __init__(self, config: GemmaConfig):
+        super().__init__()
+        self.config = config
+        self.padding_idx = config.pad_token_id
+        self.vocab_size = config.vocab_size
+        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
+        self.layers = nn.ModuleList(
+            [GemmaDecoderLayer(config, layer_idx) for layer_idx in range(config.num_hidden_layers)]
+        )
+        self.norm = GemmaRMSNorm(config.hidden_size, eps = config.rms_norm_eps)
+
+
+    def get_input_embeddings(self):
+        return self.embed_tokens
+    
+    
+    def forward(
+        self,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        kv_cache: Optional[KVCache] = None,
+    ) -> torch.FloatTensor:
+        hidden_states = inputs_embeds
+        normalizer = torch.Tensor(self.config.hidden_size ** 0.5, dtype = hidden_states.dtype)
+        hidden_states = hidden_states * normalizer
+        
+        for decoder_layer in self.layers:
+            hidden_states = decoder_layer(
+                hidden_states = hidden_states,
+                attention_mask = attention_mask,
+                position_ids = position_ids,
+                kv_cache = kv_cache,
+            )
+        
+        hidden_states = self.norm(hidden_states)
+        
+        return hidden_states
 
 
 
