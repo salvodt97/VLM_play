a30f7906f49a9feb7bcc73c136ba3c73a664e786
Salvatore Della Torca <s.dellatorca@gmail.com>
2026-04-01 14:38:27 +0200

    ADD: codifica decoder del LM

--- LLM/gemma.py
+++ LLM/gemma.py
@@ -100,9 +100,42 @@ class GemmaRMSNorm(nn.Module):
         output = output * (1 + self.weight.float())
         
         return output.type_as(x)
-
+    
+    
+    
+class GemmaDecoderLayer(nn.Module):
+    def __init__(self, config: GemmaConfig, layer_idx: int):
+        super().__init__()
+        self.hidden_size = config.hidden_size
+        self.self_attn = GemmaAttention(config = config, layer_idx = layer_idx)
+        self.mlp = GemmaMLP(config)
         
-
+        self.input_layernorm = GemmaRMSNorm(config.hidden_size, eps = config.rms_norm_eps)
+        self.post_attention_layernorm = GemmaRMSNorm(config.hidden_size, eps = config.rms_norm_eps)
+        
+        def forward(
+            self,
+            hidden_states: torch.Tensor,
+            attention_mask: Optional[torch.Tensor] = None,
+            position_ids: Optional[torch.LongTensor] = None,
+            kv_cache: Optional[KVCache] = None,
+        ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
+            residual = hidden_states
+            hidden_states = self.input_layernorm(hidden_states)  
+            hideen_states, _ = self.self_attn(
+                hidden_states = hidden_states,
+                attention_mask = attention_mask,
+                position_ids = position_ids,
+                kv_cache = kv_cache,
+            )
+            hidden_states = residual + hideen_states
+            residual = hidden_states
+            hidden_states = self.post_attention_layernorm(hidden_states)
+            hidden_states = self.mlp(hidden_states)
+            hidden_states = residual + hidden_states
+            
+            return hidden_states
+        
 class GemmaModel(nn.Module):
     def __init__(self, config: GemmaConfig):
         super().__init__()
